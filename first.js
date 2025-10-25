2d1f383b0e7f2b4e508a1565ce264cb9a1630f06
Sukhraaj <101177444+Sukhraaj@users.noreply.github.com>
2025-10-25 23:16:54 +0530

    feat: extend first.js with string manipulation example

--- first.js
+++ first.js
@@ -2,3 +2,13 @@ let str = "hello"
 let str2 = "world"
 let string = str + str2
 console.log(string)
+
+let str3 = "hello world"
+let str4 = ''
+
+for(let i = 0; i < str3.length; i++){
+    if(str3[i] !== " "){
+        str4 += str3[i]
+    }
+}
+console.log(str4)
